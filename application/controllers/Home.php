d815a09dff37993fbf32b8ae4f6b6fc59511b202
jabslumper <jabslumper@gmail.com>
2016-11-26 14:25:27 +0700

    เข้าสู่ระบบ

---
+++ application/controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lab
+ * Date: 26/11/2559
+ * Time: 13:24
+ */
+class Home extends  CI_Controller
+{
+ public function  calculator($a = "2",$b ="4")
+
+ {
+     echo $a+$b ;
+ }
+{
+
+}
+
+
+}
